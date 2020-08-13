8df41cd3222d5e8e423dc00720da71ec18887ad2
Marcel <halanocca21@gmail.com>
2020-08-13 10:49:42 -0500

    https://cloudinary.com/ donde puedes subir imagenes y imagenes primero tienes que configura upload_preset

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -4,6 +4,7 @@ import { db } from "../firebase/firebase-config";
 import {types} from '../types/types';
 import { loadNotes } from "../helpers/loadNotes";
 
+//react-journal
 
 export const startNewNotes=()=>{
 
