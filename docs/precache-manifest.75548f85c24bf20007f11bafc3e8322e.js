1d2e81b9a3b977022f2f0053829c8424b5973f8a
Marcel <halanocca21@gmail.com>
2020-08-17 18:10:05 -0500

    add docs npm run build....

---
+++ docs/precache-manifest.75548f85c24bf20007f11bafc3e8322e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c3c58a3fe2e97d1bc2c19bfe2d6c0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a93a1074879cc81ac54a",
+    "url": "/static/css/main.f1631e86.chunk.css"
+  },
+  {
+    "revision": "9e558e5c5490220b2cec",
+    "url": "/static/js/2.0f8a147b.chunk.js"
+  },
+  {
+    "revision": "31b65397dc8a13083a81efedadb0f77a",
+    "url": "/static/js/2.0f8a147b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a93a1074879cc81ac54a",
+    "url": "/static/js/main.6b5cb558.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
